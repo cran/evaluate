129dbdd2e641a0666e64d5fbf416ab7a62f72db6
Hadley Wickham <h.wickham@gmail.com>
2010-07-24 20:15:43 +0000

    version 0.1

---
+++ R/replay.r
@@ -0,0 +1,75 @@
+#' Replay a list of evaluated results.
+#' Replay a list of evaluated results, as if you'd run them in an R
+#' terminal.
+#'
+#' @param x result from \code{\link{evaluate}}
+#' @export
+#' @examples
+#' samples <- system.file("tests", package = "evaluate")
+#' replay(evaluate(file(file.path(samples, "order.r"))))
+#' replay(evaluate(file(file.path(samples, "plot.r"))))
+#' replay(evaluate(file(file.path(samples, "data.r"))))
+#' @S3method replay list
+#' @S3method replay character
+#' @S3method replay source
+#' @S3method replay warning
+#' @S3method replay message
+#' @S3method replay error
+#' @S3method replay value
+#' @S3method replay recordedplot
+replay <- function(x) UseMethod("replay", x)
+
+replay.list <- function(x) {
+  invisible(lapply(x, replay))
+}
+
+replay.character <- function(x) {
+  cat(x)
+}
+
+replay.source <- function(x) {
+  cat(line_prompt(x$src))
+}
+
+replay.warning <- function(x) {
+  message("Warning message:\n", x$message)
+}
+
+replay.message <- function(x) {
+  message(gsub("\n$", "", x$message))
+}
+
+replay.error <- function(x) {
+  if (is.null(x$call)) {
+    message("Error: ", x$message)    
+  } else {
+    call <- deparse(x$call)
+    message("Error in ", call, ": ", x$message)    
+  }
+}
+
+replay.value <- function(x) {
+  if (x$visible) print(x$value)
+}
+
+replay.recordedplot <- function(x) {
+  print(x)
+}
+
+#' Line prompt.
+#' Format a single expression as if it had been entered at the command prompt.
+#'
+#' @param x string representing a single expression
+#' @param prompt prompt for first line
+#' @param continue prompt for subsequent lines
+#' @return a string
+line_prompt <- function(x, prompt = options("prompt"), continue = options("continue")) {
+  lines <- strsplit(x, "\n")[[1]]
+  n <- length(lines)
+
+  lines[1] <- paste(prompt, lines[1], sep="")
+  if (n > 1)
+    lines[2:n] <- paste(continue, lines[2:n], sep="")    
+  
+  paste(lines, "\n", collapse="")
+}
---
+++ R/watcher.r
@@ -0,0 +1,38 @@
+#' Watch for changes in output, text and graphical.
+#'
+#' @param debug activate debug mode where output will be both printed to
+#'   screen and captured.
+#' @return list containing four functions: \code{get_new}, \code{pause}, 
+#'  \code{unpause}, \code{close}.
+watchout <- function(debug = FALSE) {
+  output <- vector("character")
+  prev   <- vector("character")
+
+  con <- textConnection("output", "wr", local=TRUE)
+  sink(con, split = debug)
+  
+  list(
+    get_new = function() {
+      out <- list()
+      if (length(output) != length(prev)) {
+        new <- output[setdiff(seq_along(output), seq_along(prev))]
+        prev <<- output
+        
+        out$text <- paste(paste(new, collapse="\n"), "\n", sep = "")
+      }
+
+      graphics <- plot_snapshot()
+      if (!is.null(graphics)) {
+        out$graphics <- graphics
+      }
+      unname(out)
+    },
+    pause = function() sink(),
+    unpause = function() sink(con, split = debug),
+    close = function() {
+      sink()
+      close(con)
+      output
+    }
+  )
+}
