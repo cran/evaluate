3367c3b6de4ca9c0dc4e1793393146d9e6966905
Yihui Xie <xie@yihui.name>
2019-05-28 14:50:02 +0000

    version 0.14

--- R/hooks.r
+++ R/hooks.r
@@ -15,14 +15,38 @@
 #' set_hooks(hooks, "replace")
 #' plot(1)
 set_hooks <- function(hooks, action = "append") {
-  stopifnot(is.list(hooks))
-  stopifnot(!is.null(names(hooks)) && all(names(hooks) != ""))
-
   old <- list()
   for (hook_name in names(hooks)) {
     old[[hook_name]] <- getHook(hook_name)
     setHook(hook_name, hooks[[hook_name]], action = action)
   }
-
   invisible(old)
 }
+
+#' Remove hooks.
+#'
+#' This provides a way to remove previously set hook values.
+#'
+#' @inheritParams set_hooks
+#' @keywords internal
+#' @export
+#' @examples
+#' new1 <- list(before.plot.new = function() print("Plotted!"))
+#' new2 <- list(before.plot.new = function() print("Plotted Again!"))
+#' set_hooks(new1)
+#' set_hooks(new2)
+#' plot(1)
+#' remove_hooks(new1)
+#' plot(1)
+#' remove_hooks(new2)
+#' plot(1)
+remove_hooks <- function(hooks) {
+  for (hook_name in names(hooks)) {
+    hook <- getHook(hook_name)
+    for (fun in unlist(hooks[hook_name])) {
+      hook[sapply(hook, identical, fun)] <- NULL
+    }
+    setHook(hook_name, hook, "replace")
+  }
+}
+
